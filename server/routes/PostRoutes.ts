f2a5d1bebba976008d2e9286b7461fa04ba7242e
khalid <rabaaouikhalid@gmail.com>
2025-09-10 16:08:32 +0200

    add docker!?!

--- server/routes/PostRoutes.ts
+++ server/routes/PostRoutes.ts
@@ -4,9 +4,9 @@ import {getPostById ,getPosts, getAreaPosts,findPost   } from '../controllers/po
 const router = Router();
 
 router.get('/getById/:id', getPostById);
-router.get('/getAll/', getPosts);
-router.get('/getArea/', getAreaPosts);
-router.get('/search/',findPost );
+router.get('/getAll', getPosts);
+router.get('/getArea', getAreaPosts);
+router.get('/search',findPost );
 
 
 export default router;
